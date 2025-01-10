e7b89ca4e90f42e664cfba32ca136d7e5ba44b8b
thebearguy <bugbreaker18@gmail.com>
2025-01-10 13:46:14 +0530

    account transactions, routes setup

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 const  express = require("express");
-
+const cors = require("cors");
+const rootRouter = require("./routes/index");
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1", rootRouter);
 
-
-app.get('/', (req, res) => {
-    res.send("I'm up")
-})
 
 app.listen(
     3000,
     () => {
-        console.log("hello there");
+        console.log("hello there, I'm running at PORT :: 3000 ");
     }
 )
--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authMiddleware = require("../middleware");
-const {Account} = require("../db");
+const {Account, User} = require("../db");
+const { default: mongoose } = require("mongoose");
 
 const router = express.Router();
 
@@ -16,4 +17,39 @@ router.get('/balance', authMiddleware, async (req, res, next) => {
 router.post('/transfer', authMiddleware, async (req, res, next) => {
     // db transaction
     // user should not pay an amount they dont possess
+    // Step 3: Use withTransaction to start a transaction, execute the callback, and commit (or abort on error)
+  // Note: The callback for withTransaction MUST be async and/or return a Promise.
+    const session = mongoose.startSession();
+
+    try {
+        (await session).startTransaction();
+        const {amount, to} = req.body;
+        const account = await  Account.findOne({userId: req.userId}).session(session);
+        if (!account || amount > account.balance) {
+            (await session).abortTransaction();
+            return res.status(400).json({
+                msg: "Insufficient Balance"
+            })
+        }
+        const toAccount = Account.findOne({userId: to}).session(session);
+        if (!toAccount) {
+            (await session).abortTransaction();
+            return res.status(400).json({
+                msg: "Account to pay the money is invalid"
+            })
+        }
+
+        // transfer
+        Account.updateOne({userId: req.userId}, { $inc: { balance: -amount }}).session(session);
+        Account.updateOne({userId: to}, { $inc: { balance: amount}}).session(session);
+        //commit
+        (await session).commitTransaction();
+    } catch{
+        async (e) => await session.abortTransaction();
+        console.log(e);
+    }
+    // With transactionAsyncLocalStorage, you no longer need to pass sessions to every operation. Mongoose will add the session by default under the hood.
 })
+
+
+module.exports = router; 
